ba9b75a4f2a5957de2197942fee3ab3c62597621
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-07-16 13:44:19 +0200

    error types in P2, resolve #13

--- interface/src/waiter_p2/waiter_displayer.ts
+++ interface/src/waiter_p2/waiter_displayer.ts
@@ -10,14 +10,6 @@ export class WaiterDisplayer {
 
             <div class='waiter_p2_response'>
                 <div>
-                <div>Was translated:</div>
-                <div class='waiter_p2_response_single'>
-                    <input id='val_${index}_trans' index='${index}' ${mtRating.translated == undefined ? '' : 'trigger'} class='synctext' type='checkbox' value='${mtRating.translated ?? -1}'>
-                    <div id='val_${index}_trans_text'>-</div>
-                </div>
-                </div>
-
-                <div>
                 <div>Fluency:</div>
                 <div class='waiter_p2_response_single'>
                     <input id='val_${index}_fluency' index='${index}' ${mtRating.fluency == undefined ? '' : 'trigger'} class='synctext' type='range' min='0' , max='1' , step='0.1' value='${mtRating.fluency ?? -1}'>
@@ -34,22 +26,47 @@ export class WaiterDisplayer {
                 </div>
 
                 <div>
-                <div>Mistakes:</div>
-                <div class='waiter_p2_response_single'>
-                    <textarea id='val_${index}_errors' index='${index}' ${mtRating.errors == undefined ? '' : 'trigger'} class='synctext'>${mtRating.errors ?? ''}</textarea>
+                <div>Error type:</div>
+                <div class='waiter_p2_error_type'>
+                    <input type='checkbox' id='nontranslated_${index}'>
+                    <label for='nontranslated_${index}'>Not translated</label>
+                    <br>
+                    <input type='checkbox' id='tootranslated_${index}'>
+                    <label for='tootranslated_${index}'>Over-translated</label>
+                    <br>
+                    <input type='checkbox' id='terminology_${index}'>
+                    <label for='terminology_${index}'>Bad terminology</label>
+                    <br>
+                    <input type='checkbox' id='style_${index}'>
+                    <label for='style_${index}'>Bad style</label>
+                    <br>
+                    <input type='checkbox' id='sense_${index}'>
+                    <label for='sense_${index}'>Distorted sense</label>
+                    <br>
+                    <input type='checkbox' id='typography_${index}'>
+                    <label for='typography_${index}'>Bad typography</label>
+                    <br>
+                    <input type='checkbox' id='grammar_${index}'>
+                    <label for='grammar_${index}'>Grammar</label>
                 </div>
+                </div>
+                
+                <!--
+                <div>
+                    <div>Mistakes:</div>
+                    <div class='waiter_p2_response_single'>
+                        <textarea id='val_${index}_errors' index='${index}' ${mtRating.errors == undefined ? '' : 'trigger'} class='synctext'>${mtRating.errors ?? ''}</textarea>
+                    </div>
                 </div>
 
-                <!--
                 <div>
                     <div>Non-conflicting?</div>
                     <div class='waiter_p2_response_single'>
                         <input id='val_${index}_nonconf' index='${index}' class='synctext' type='range' min='0' , max='1' , step='0.1' value='-1'>
                         <div id='val_${index}_nonconf_text'>-</div>
                     </div>
-                    </div>
-                    -->
-                <br>
+                </div>
+                -->
             </div>
 
             <br>
