6d1f77f09423beee020c1e70076b4198756510f2
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-07-23 16:51:11 +0200

    split promt and etranslation

--- processing/src/results/load.py
+++ processing/src/results/load.py
@@ -2,7 +2,9 @@
 
 import json
 import argparse
-import glob, os
+import glob
+import os
+import re
 from pathlib import Path
 
 def load_single():
@@ -10,21 +12,36 @@ def load_single():
     parser.add_argument('rating_file', help='Path to the logged user JSONfile')
     args = parser.parse_known_args()[0]
 
-    data = json.load(open(args.rating_file, 'r'))
-    return data
+    return list(load_files([args.rating_file]).values())[0]
 
 def load_all():
     parser = argparse.ArgumentParser(description='Loads ')
-    parser.add_argument('rating_dir', help='Path to the logged user JSONfile directory')
+    parser.add_argument(
+        'rating_dir', help='Path to the logged user JSONfile directory')
     args = parser.parse_known_args()[0]
 
     files = glob.glob(f'{args.rating_dir}/*.json')
-
     return load_files(files)
+
+def replaceDoc(key, doc):
+    if re.match(r'\w+e', key):
+        # EN->CS only eTranslation
+        def replaceLine(line):
+            return {(k.replace( 'PROMT_NMT-eTranslation', 'eTranslation')): v for (k, v) in line.items()}
+    elif re.match(r'\w+c', key):
+        # CS->EN only PROMT_NMT
+        def replaceLine(line):
+            return {(k.replace( 'PROMT_NMT-eTranslation', 'PROMT_NMT')): v for (k, v) in line.items()}
+    else:
+        def replaceLine(line):
+            return line
+    return {k: replaceLine(v) for (k, v) in doc.items()}
 
 def load_files(files):
     out = {}
     for file in files:
         name = Path(file).stem
-        out[name] = json.load(open(file, 'r'))
+        obj = json.load(open(file, 'r'))
+        obj = {k:replaceDoc(k, v) for (k, v) in obj.items()}
+        out[name] = obj
     return out
--- processing/src/results/time_spent.py
+++ processing/src/results/time_spent.py
@@ -1,33 +1,42 @@
 #!/usr/bin/env python3
 
-from load import load_single
-
-data = load_single()
-
-times = []
-for docmodel in data.values():
-    for secsent in docmodel.values():
-        times.append(secsent['time'])
-times.sort()
-
-print('Entries:', len(times))
-
-if len(times) <= 1:
-    print('Too little number of entries')
-    exit(0)
+from load import load_all
 
 
-spentTime = 0
-blockBegin = times[0]
-blocks = 0
+def time_single(name, data):
+    times = []
+    for docmodel in data.values():
+        for secsent in docmodel.values():
+            times.append(secsent['time'])
+    times.sort()
 
-for index, time in enumerate(times[1:], start=1):
-    diff = time - times[index-1]
-    # 20 minutes for one view
-    if diff > 20*60*1000 or index == len(times)-1:
-        spentTime += times[index-1] - blockBegin
-        blockBegin = time
-        blocks += 1
 
-print('Blocks:', blocks)
-print('Time spent: ', spentTime//1000, 's', sep='')
+    if len(times) <= 1:
+        print(f'{name}: {len(times)} entry')
+        print('Too little number of entries')
+        return
+
+    spentTime = 0
+    blockBegin = times[0]
+    blocks = 0
+
+    for index, time in enumerate(times[1:], start=1):
+        diff = time - times[index-1]
+        # 20 minutes for one view
+        if diff > 20*60*1000 or index == len(times)-1:
+            spentTime += times[index-1] - blockBegin
+            blockBegin = time
+            blocks += 1
+
+    print(f'{name}: {len(times)} entries, {blocks} blocks')
+    print(f'{spentTime/1000:5.0f}s',
+          f'{spentTime/1000/60:3.0f}min ',
+          f'{spentTime/1000/60/60:3.2f}h',
+          )
+
+
+dataAll = load_all()
+
+for (name, data) in dataAll.items():
+    time_single(name, data)
+    print()
